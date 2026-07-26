cb542ec33ba41bb1fef78701507ea1e51352461f
Ademario Santana <ademario63@gmail.com>
2026-07-26 18:32:44 -0300

    feat: open app after to click in notification

--- android/app/src/main/java/com/tccdesconecta/screentime/ScreenTimeForegroundService.kt
+++ android/app/src/main/java/com/tccdesconecta/screentime/ScreenTimeForegroundService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.app.usage.UsageStatsManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ServiceInfo
@@ -16,6 +17,7 @@ import android.os.Looper
 import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.core.app.NotificationCompat
+import com.desconecta.MainActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import com.desconecta.R
 import java.text.SimpleDateFormat
@@ -227,10 +229,21 @@ class ScreenTimeForegroundService : Service() {
     }
 
     private fun buildNotification(contentText: String): Notification {
+        val openIntent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, openIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        //TODO: está abrindo o app mas está indo pra última tela em que o app estava aberto, precisa ir pra main screen
+
         val notification = NotificationCompat.Builder(this, ScreenTimeBackgroundConfig.NOTIFICATION_CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle("Desconecta monitorando")
             .setContentText(contentText)
+            .setContentIntent(pendingIntent)
             .setOngoing(true)
             .setAutoCancel(false)
             .setCategory(NotificationCompat.CATEGORY_SERVICE)
